ac554693dc9da88db181c27626e9bb5b394c9605
Tim Levett <levett@wisc.edu>
2014-07-02 16:00:56 -0500

    NOJIRA : Add in test case for hello world

---
+++ notification-server/src/test/java/edu/wisc/notification/NotificationServerApplicationTest.java
@@ -0,0 +1,38 @@
+package edu.wisc.notification;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NotificationServerApplication.class)
+@WebAppConfiguration
+public class NotificationServerApplicationTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void testHome() throws Exception {
+
+        this.mvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().string("{\n  \"test\" : \"Hello World\"\n}"));
+    }
+}
